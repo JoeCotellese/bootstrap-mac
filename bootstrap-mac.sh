831bf36b25760ddf76ec75a6ae91f22fee2cf470
Joe Cotellese <joe@cotellese.net>
2024-03-07 17:33:06 -0500

    Update bootstrap-mac.sh

--- bootstrap-mac.sh
+++ bootstrap-mac.sh
@@ -2,13 +2,10 @@
 
 # Homebrew Script for OSX
 # To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
-# To execute directly from github:
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing brew..."
-#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-#eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "Installing brew cask..."
 brew tap homebrew/cask
